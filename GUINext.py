414c2bd056cb43e5beae6a25a061355aba30e777
632633533@qq.com <quaff,lyu@gmail.com>
2020-03-12 14:22:53 -0700

    test

--- GUINext.py
+++ GUINext.py
@@ -47,20 +47,8 @@ class Ui_Dialog(object):
         pg.setConfigOption('background', '#17191A')
         self.k_widget = QtWidgets.QWidget(self.graph_widget)
         self.k_widget.setMinimumSize(QtCore.QSize(500, 200))
-        self.k_widget.setObjectName("pie_widget")
-        self.k_layout = QtWidgets.QGridLayout()  # grid
-        self.k_widget.setLayout(self.k_layout)  # candlestick
-        self.k_plt = pg.PlotWidget()  # 实例化一个绘图部件
-        self.k_layout.addWidget(self.k_plt)  # 添加绘图部件到K线图部件的网格布局层
-
-        # 将上述部件添加到布局层中
-        # self.main_layout.addWidget(self.stock_code, 0, 0, 1, 1)
-        # self.main_layout.addWidget(self.option_sel, 0, 1, 1, 1)
-        # self.main_layout.addWidget(self.que_btn, 0, 2, 1, 1)
-        # self.main_layout.addWidget(self.k_widget, 1, 0, 3, 3)
 
 
-        self.horizontalLayout_2.addWidget(self.k_widget)
         spacerItem = QtWidgets.QSpacerItem(100, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem)
         self.verticalLayout.addWidget(self.graph_widget)
@@ -89,15 +77,15 @@ class Ui_Dialog(object):
         # self.graph_widget = QtWidgets.QWidget(self.canvas)
         self.graph_widget.setObjectName("graph_widget")
 
-        # self.pie_widget =  QtWidgets.QWidget(self.graph_widget)
+        self.pie_widget =  QtWidgets.QWidget(self.graph_widget)
         # sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         # sizePolicy.setHorizontalStretch(0)
         # sizePolicy.setVerticalStretch(0)
         # sizePolicy.setHeightForWidth(self.pie_widget.sizePolicy().hasHeightForWidth())
         # self.pie_widget.setSizePolicy(sizePolicy)
-        # self.pie_widget.setMinimumSize(QtCore.QSize(0, 50))
-        # self.pie_widget.setMaximumSize(QtCore.QSize(16777215, 50))
-        self.k_widget.setObjectName("pie_widget")
+        self.pie_widget.setMinimumSize(QtCore.QSize(0, 50))
+        # # self.pie_widget.setMaximumSize(QtCore.QSize(16777215, 50))
+        # self.k_widget.setObjectName("pie_widget")
         # self.pie_widget.setStyleSheet("#pie_widget{background-color: yellow}")
         # beautify window
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # hide the boarder
@@ -170,13 +158,6 @@ class Ui_Dialog(object):
         self.plot.addItem(self.hLine, ignoreBounds=True)
         self.vb = self.plot.vb
 
-        # self.proxy = pg.SignalProxy(self.plot.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
-
-
-        self.data = data
-        # self.generatePicture()
-        self.plot_k_line()
-
 
     def paint(self, p, *args):
         p.drawPicture(0, 0, self.picture)
@@ -184,44 +165,6 @@ class Ui_Dialog(object):
     def boundingRect(self):
         return QtCore.QRectF(self.picture.boundingRect())
 
-
-    def plot_k_line(self,code=None,start=None,end=None):
-        self.data = np.array(data)
-
-        # data = [[0, 10, 20, 9, 23], [1, 10, 20, 9, 23], [2, 10, 20, 9, 23], [3, 10, 20, 9, 23]]
-        y_min = self.data[:,3].min()
-        # print(y_min)
-        y_max = self.data[:,4].max()
-
-        self.k_plt.plotItem.clear() # 清空绘图部件中的项
-        # item = self.picture  # 生成蜡烛图数据
-        item = CandlestickItem(self.data)  # 生成蜡烛图数据
-        self.k_plt.addItem(item)  # 在绘图部件中添加蜡烛图项目
-        self.k_plt.showGrid(x=True, y=True)  # 设置绘图部件显示网格线
-        self.k_plt.setYRange(y_min,y_max)
-        self.k_plt.setLabel(axis='left', text='score')  # 设置Y轴标签
-        self.k_plt.setLabel(axis='bottom', text='date')  # 设置X轴标签
-        self.label = pg.TextItem()  # 创建一个文本项
-        self.k_plt.addItem(self.label)  # 在图形部件中添加文本项
-
-        self.vLine = pg.InfiniteLine(angle=90, movable=False, )  # 创建一个垂直线条
-        self.hLine = pg.InfiniteLine(angle=0, movable=False, )  # 创建一个水平线条
-        self.k_plt.addItem(self.vLine, ignoreBounds=True)  # 在图形部件中添加垂直线条
-        self.k_plt.addItem(self.hLine, ignoreBounds=True)  # 在图形部件中添加水平线条
-
-    def mousePressEvent(self,event):
-        if event.button() == QtCore.Qt.LeftButton:
-            self.mPos = event.pos()
-        event.accept()
-
-    def mouseReleaseEvent(self, event):
-        self.mPos = None
-        event.accept()
-
-    def mouseMoveEvent(self,event):
-        if event.buttons() == QtCore.Qt.LeftButton and self.mPos:
-            self.windowMoved.emit(self.mapToGlobal(event.pos() - self.mPos))
-        event.accept()
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -230,31 +173,3 @@ class Ui_Dialog(object):
 
 
 
-# K线图绘制类
-class CandlestickItem(pg.GraphicsObject):
-    # 州的先生zmister.com
-    def __init__(self, data):
-        pg.GraphicsObject.__init__(self)
-        self.data = data  # data里面必须有以下字段: 时间, 开盘价, 收盘价, 最低价, 最高价
-        self.generatePicture()
-
-    def generatePicture(self):
-        self.picture = QtGui.QPicture() # 实例化一个绘图设备
-        p = QtGui.QPainter(self.picture) # 在picture上实例化QPainter用于绘图
-        p.setPen(pg.mkPen('w')) # 设置画笔颜色
-        w = (self.data[1][0] - self.data[0][0]) / 3.
-        for (t, open, close, min, max) in self.data:
-            # print(t, open, close, min, max)
-            p.drawLine(QtCore.QPointF(t, min), QtCore.QPointF(t, max)) # 绘制线条
-            if open > close: # 开盘价大于收盘价
-                p.setBrush(pg.mkBrush('g')) # 设置画刷颜色为绿
-            else:
-                p.setBrush(pg.mkBrush('r')) # 设置画刷颜色为红
-            p.drawRect(QtCore.QRectF(t - w, open, w * 2, close - open)) # 绘制箱子
-        p.end()
-
-    def paint(self, p, *args):
-        p.drawPicture(0, 0, self.picture)
-
-    def boundingRect(self):
-        return QtCore.QRectF(self.picture.boundingRect())
