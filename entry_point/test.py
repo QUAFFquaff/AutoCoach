b69c6c51aa65539fe0e19657411f96b415df84cd
DabenW <wdb747534116@outlook.com>
2020-03-12 14:34:59 -0700

    merge

---
+++ entry_point/test.py
@@ -0,0 +1,14 @@
+
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class FeedbackController(QThread):
+    def __init__(self, feedback_buffer):
+        super().__init__()
+        self.feedback_buffer = feedback_buffer
+
+    def run(self):
+
+
+
+class
